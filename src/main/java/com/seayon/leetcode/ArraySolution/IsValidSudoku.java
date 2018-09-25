f9f320cfb832b50d22b4d76b0540b124a30f8493
Seayon <153660639@qq.com>
2018-09-25 14:05:51 +0800

    36. 有效的数独

--- src/main/java/com/seayon/leetcode/ArraySolution/IsValidSudoku.java
+++ src/main/java/com/seayon/leetcode/ArraySolution/IsValidSudoku.java
@@ -3,6 +3,7 @@ package com.seayon.leetcode.ArraySolution;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.HashMap;
 
 /**
  * @Version 1.0
@@ -12,16 +13,43 @@ import java.util.Arrays;
  */
 public class IsValidSudoku {
 	public boolean isValidSudoku(char[][] board) {
+		HashMap<String, HashMap<Character, Integer>> hashMapHashMap = new HashMap<>();
+		for (int i = 0; i < board.length; i++) {
+			char[] in = board[i];
+			for (int j = 0; j < in.length; j++) {
+				if (!hashMapHashMap.containsKey("row" + i)) {
+					hashMapHashMap.put("row" + i, new HashMap<>());
+				}
+				if (!hashMapHashMap.containsKey("column" + j)) {
+					hashMapHashMap.put("column" + j, new HashMap<>());
+				}
+				int bi = i / 3;
+				int bj = j / 3;
+				if (!hashMapHashMap.containsKey("box" + bi + bj)) {
+					hashMapHashMap.put("box" + bi+bj, new HashMap<>());
+				}
+				HashMap<Character, Integer> row = hashMapHashMap.get("row" + i);
+				HashMap<Character, Integer> column = hashMapHashMap.get("column" + j);
+				HashMap<Character, Integer> box = hashMapHashMap.get("box" + bi + bj);
 
+				if ('.'!=board[i][j]&&(row.containsKey(board[i][j]) || column.containsKey(board[i][j]) || box.containsKey(board[i][j]))) {
+					return false;
+				} else {
+					row.put(board[i][j], i);
+					column.put(board[i][j], j);
+					box.put(board[i][j], i);
+				}
+			}
+		}
 		return true;
 	}
 
 	@Test
 	public void test() {
 		char[][] a = {
-				{'5', '3', '.', '.', '7', '.', '.', '.', '.'},
-				{'6', '.', '.', '1', '9', '5', '.', '.', '.'},
-				{'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+				{'.', '.', '.', '.', '7', '.', '.', '.', '.'},
+				{'.', '.', '.', '1', '9', '5', '.', '.', '.'},
+				{'.', '.', '8', '.', '.', '.', '.', '6', '.'},
 				{'8', '.', '.', '.', '6', '.', '.', '.', '3'},
 				{'4', '.', '.', '8', '.', '3', '.', '.', '1'},
 				{'7', '.', '.', '.', '2', '.', '.', '.', '6'},
@@ -29,5 +57,7 @@ public class IsValidSudoku {
 				{'.', '.', '.', '4', '1', '9', '.', '.', '5'},
 				{'.', '.', '.', '.', '8', '.', '.', '7', '9'}
 		};
+		System.out.println(isValidSudoku(a));
+
 	}
 }
